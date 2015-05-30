da59db92663d3d514b66271c747b568ede7612be
Firas Moosvi <firas@moosvi.com>
2015-05-29 22:31:38 -0700

    initial commit of working code

---
+++ interclass.py
@@ -0,0 +1,310 @@
+import pandas
+import scipy.stats
+from collections import Counter
+import collections
+import math
+import numpy
+from collections import defaultdict
+
+
+
+def renameFields(dataFrame,
+                 origPhrases = None,
+                 newPhrases = None):
+    """
+    Purpose: This function replaces the provided original columns (origPhrases)with the provided phrases (newPhrases) in a Pandas data frame (dataFrame).
+
+    If origPhrases is not provided, default values are provided based on experience
+
+    Note that if the length of newPhrases is not the same as the length of 
+    origPhrases, it will be assumed that newPhrases is of length one, and 
+    the function first tries to make it the same length as origPhrases.
+
+    Then, it is asserted that the length of newPhrases and origPhrases is
+    the same, otherwise and error will be thrown.
+    """
+
+    
+    if origPhrases is None:
+        origPhrases = ['Please enter your 8 digit student ID number:',
+                       'Please enter your student ID number (8 digits)',
+                       'Username',
+                       'Student ID']
+        
+    if newPhrases is None:
+        newPhrases = ['StudentNumber']*len(origPhrases)
+        
+    elif len(newPhrases) != len(origPhrases):
+        newPhrases = [newPhrases]*4
+        
+    assert len(newPhrases) == len(origPhrases),'Please input either one replacement, or match length of origPhrases'
+        
+    for o,n in zip(origPhrases,newPhrases):
+        dataFrame.rename(columns={o:n}, inplace=True)
+        
+    return dataFrame
+
+def checkQuestions(dataFrame,
+                   questionDict,
+                   fname):
+    """
+    Purpose: This function checks the pandas data frame (dataFrame) to make sure that all the interCLASS question statements are stored correctly and in the same format. The function checks spelling, punctuation, capitalization. 
+
+    The questions are stored in questionDict, and the check works by trying to access dataFrame columns from a dictionary of questions (questionDict).
+
+    Failed question statements are printed, and so is the filename (fname) for the offending excel file.
+
+    """
+    
+    # Check to make sure that Q statements are stored the same way for all datasets
+
+    check_questions = True
+    
+    for q in questionDict.values():
+        try:
+            x=dataFrame[q]
+        except:
+            check_questions = False
+            print('{0} failed {1}'.format(dl,q))
+
+    if check_questions:
+        print('#### {0} #### \n All systems go, question statements are validated\n'.format(fname))
+    else:
+        print('#### {0} #### \n There were problems, please address them.\n'.format(fname))    
+
+def likertReplace(dataFrame,
+                  likertPhrase = None,
+                  scores = None):
+    """
+    Purpose: This function replaces the likert phrases (likertPhrase) in a data frame (dataFrame) into numbers (scores)
+
+    If no likertPhrases are provided, default 5 default values are provided, mapped as follows:
+
+    'Strongly Disagree'     = -1
+    'Disagree'              = -1
+    'Neutral'               = 0
+    'Agree'                 = 1
+    'Strongly Agree'        = 1
+    '<Unanswered>'          = numpy.nan
+    """
+    if likertPhrase is None:
+        likertPhrase = ['Strongly Disagree', 
+                        'Disagree', 
+                        'Neutral', 
+                        'Agree', 
+                        'Strongly Agree',
+                        '<Unanswered>']
+
+    # Choose Default scoring of the likert scale
+    if scores is None:     
+        scores = [-1,-1,0,1,1,numpy.nan]
+
+    # Replace likert scale with scores as specified    
+    dataFrame = dataFrame.replace(to_replace=likertPhrase, value=scores)
+    
+    return dataFrame
+    
+def loadExcelasDF(rawPath= None,
+                  filenames = None,
+                  sheetname = 0,
+                  scores = None,
+                  questionDict = None,
+                  replaceLikert = False):
+    """
+    Purpose: This function loads provided excel files (filenames) as a dataFrame.
+    """
+    # Specify the rawPath of the csv files
+    if rawPath is None:
+        rawpath = '/phd/TA/Year 3 - InterClass/python/raw/'
+    
+    # Initialize empty df 
+    allData = pandas.DataFrame()    
+    
+    # Iterate through all the filenames and append them to a single Data Frame    
+    for fname in filenames:
+
+        # Load in the dataframe from the csv file
+        df = pandas.io.excel.read_excel(rawpath+fname,sheetname) 
+    
+        # Rename the student ID fileds
+        df = renameFields(df)
+    
+        # Check the questions for each df
+        if questionDict is not None:
+            checkQuestions(df,questionDict,fname)
+        else:
+            print('Warning: questions have not been checked')
+        
+        # Append them to one big dataframe
+        allData = allData.append(df)    
+        
+    # Set the index of the dataframe to be StudentNumber
+    allData.set_index('StudentNumber',inplace=True)
+    
+    if replaceLikert:
+        # Replace Likert scale with scores
+        allData = likertReplace(allData,scores=scores)
+        
+    return allData
+
+def makePlotDict(allData,
+                 questionDict,
+                 courseList):
+
+    """
+    Purpose: This function takes a complete dataFrame (allData) and produces a ready-made dictionary (plotDict) that can be plotted or printed as needed.
+
+    The function loops through the provided questions (questionDict) and courses (courseList) and stores the mean, standard error, and number of students
+    """
+    
+    # Initializing the dictionary
+    plotDict = collections.OrderedDict()
+    for u in questionDict.keys():
+        plotDict[u] = {}
+
+    # Filling the dictionary with mean, sem, N
+    for y,x in questionDict.iteritems():
+        for c in courseList:
+            currD = allData[allData['Course']==c]        
+
+            # Screening for empty numbers/blanks, to prevent nans
+            testDat = currD[x]
+            testDat = testDat[numpy.isfinite(testDat)]
+
+            # Calculating and storing the mean, sem, and N for the dataset
+            m = round(scipy.stats.nanmean(testDat),2)
+            s = round(scipy.stats.sem(testDat),2)
+            n = len(testDat)
+            plotDict[y].update({c:[m,s,n]})
+            
+    return plotDict
+
+def arrowPlotData(plotDict,
+                  desiredCourses,
+                  questionDictKeys,
+                  delta = None):
+
+    """
+    Purpose: This function takes a ready-made dictionary (plotDict), and the courses of interest (desiredCourses), and the questions of interest (questionDictKeys), and returns two dictionaries that can be provided to arrowPlot to construct the arrowPlot
+
+    The delta flag allows you to create an arrow plot with the second course data subtracted from the first course.
+    """
+
+    if delta is True:
+        assert len(desiredCourses) == 2, 'Please make sure a pair of courses is provided'
+        
+        mean = []
+
+        for q in questionDictKeys:        
+            mean.append(plotDict[q][desiredCourses[1]][0] -\
+                        plotDict[q][desiredCourses[0]][0])
+            
+        return mean
+
+    else:
+        # Initializing the dictionaries
+        plotGenerator = collections.OrderedDict()
+        plotGeneratorErr = collections.OrderedDict()
+
+        for u in desiredCourses:
+            plotGenerator[u] = []
+            plotGeneratorErr[u] = []
+
+        mean = {}
+        sem = {}
+
+        for c in desiredCourses:
+            for q in questionDictKeys:        
+                plotGenerator[c].append(plotDict[q][c][0])
+                plotGeneratorErr[c].append(plotDict[q][c][1])
+            mean.update({c: plotGenerator[c]})
+            sem.update({c: plotGeneratorErr[c]})
+
+        return mean,sem
+
+def individualStudents(allData,
+                       desiredCourses,
+                       questionValue):
+
+    """
+    Purpose: This function takes a complete dataFrame (allData), courses of interest (desiredCourses), and a single question (questionValue) to return the difference between invidual students as a dictionary
+
+    Notably, the function checks and uses only students that are present in both courses. At this stage, the differences are all provided and the dictionary keys are the pair of values '[-1.0, 1.0]' with the first value from the first course in desiredCourses and the second value from the second course in desiredCourses (i.e. Post - Pre)
+
+
+    """
+    
+    assert len(desiredCourses) == 2, 'Please make sure a pair of courses is provided'
+    
+    tmp = []
+    
+    # Populate tmp
+    for c in desiredCourses:
+        currD = allData[allData['Course']==c]        
+
+        # Screening for empty numbers/blanks, to prevent nans
+        testDat = currD[questionValue]
+        testDat = testDat[numpy.isfinite(testDat)]
+
+        tmp.append(testDat)
+    
+    # Find students that exist in both pre and post tests    
+
+    preStudents = list(tmp[0].index)
+    postStudents = list(tmp[1].index)
+    
+    # Get the ones that exist in both
+    prePostStudents = list(set(preStudents) & set(postStudents))
+    
+    # Initialize the comparisons dict
+    comparisons = {}
+
+    for i in prePostStudents:
+        comparisons[i] = []
+        
+    # Get the comparisons and store them 
+
+    for s,pre,post in zip(list(tmp[0].index),list(tmp[0]),list(tmp[1])):
+
+        comparisons[s] = [pre,post]
+        
+        
+    # Find out the number of students doing one thing or another
+    
+    diff = defaultdict(int)
+    for l in comparisons.values():
+        diff[str(l)] += 1    
+
+    return diff
+
+def collapseChanges(result):
+    
+    inc = 0
+    dec = 0
+    sm = 0
+
+    for k,v in result.iteritems():
+        
+        # Convert the ridiculous string into a list of ints
+
+        if len(k) > 3 : # This is to avoid messages for blank values
+            try:
+                st = k[1:-1].split(',')
+                k = [float(it) for it in st]
+
+            except ValueError:
+                print('The key you are using: ({0}) is invalid'.format(k))
+                raise
+                
+            else:
+                # Check to see if the differences are increasing, decreasing or staying the same 
+                if k[1] - k[0] > 0:
+                    inc+=v
+                elif k[1] - k[0] < 0:
+                    dec+=v 
+                elif k[1] - k[0] == 0:
+                    sm+=v
+
+    return {'Increase':inc,
+            'Decrease':dec,
+            'Same':sm}    
---
+++ interclassPlots.py
@@ -0,0 +1,303 @@
+import pylab
+
+def arrowPlot(val,
+			  valerr=None,
+			  title=None,
+			  rawPath=None,
+			  filename='template.png'):
+
+	if title is None:
+		title = filename
+
+	if rawPath is None:
+		rawpath = '/phd/TA/Year 3 - InterClass/python/export/'
+
+	# These are the errors set up as strings
+
+	if valerr is None:
+		stringError = ['' for err in val]
+	else:
+		stringError = ['+/- '+str(err) for err in valerr]    	
+
+	pylab.figure(figsize=(12,6))
+	ax = pylab.subplot(111)
+
+
+	# Create the boxes for each of the subjects
+
+	ann = ax.annotate("Chemistry",
+	                  xy=(0.30, 0.4), xycoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w")
+	                  )
+
+	ann2 = ax.annotate("Biology",
+	                  xy=(0.5, 0.82), xycoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w")
+	                  )
+
+	ann3 = ax.annotate("Physics",
+	                  xy=(0.7, 0.4), xycoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w")
+	                  )
+	pylab.xlim(0,1)
+	pylab.ylim(0,1)
+	pylab.grid()
+
+
+	# Creating the arrows in the plot
+
+	# Chem to Bio
+	ax.annotate("",
+	                  xy=(.43, 0.83),xycoords='data',
+	                  xytext=(0.3, 0.47), textcoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w"),
+
+	                  arrowprops=dict(arrowstyle="-|>",
+	                                  connectionstyle="angle3",
+	                                  fc="w"), 
+	                  )
+
+	# Bio to Chem
+	ax.annotate("",
+	                  xy=(0.3, 0.47),xycoords='data',
+	                  xytext=(.43, 0.83), textcoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w"),
+
+	                  arrowprops=dict(arrowstyle="-|>",
+	                                  connectionstyle="angle3",
+	                                  fc="w"), 
+	                  )
+
+
+	# Bio to Physics
+	ax.annotate("",
+	                  xy=(0.7, 0.47),xycoords='data',
+	                  xytext=(0.57, 0.83), textcoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w"),
+
+	                  arrowprops=dict(arrowstyle="-|>",
+	                                  connectionstyle="angle3",
+	                                  fc="w"), 
+	                  )
+
+	# Physics to Bio
+	ax.annotate("",
+	                  xy=(0.57, 0.83),xycoords='data',
+	                  xytext=(.7, 0.47), textcoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w"),
+
+	                  arrowprops=dict(arrowstyle="-|>",
+	                                  connectionstyle="angle3",
+	                                  fc="w"), 
+	                  )
+
+	# Physics to Chem
+	ax.annotate("",
+	                  xy=(0.39, 0.39),xycoords='data',
+	                  xytext=(0.63, 0.39), textcoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w"),
+
+	                  arrowprops=dict(arrowstyle="-|>",
+	                                  connectionstyle="angle3,angleA=-45,angleB=45",
+	                                  fc="w"), 
+	                  )
+
+	# Chem to Phys
+	ax.annotate("",
+	                  xy=(0.63, 0.39),xycoords='data',
+	                  xytext=(.39, 0.39), textcoords='axes fraction',
+	                  size=20, va="center", ha="center",
+	                  bbox=dict(boxstyle="round", fc="w"),
+
+	                  arrowprops=dict(arrowstyle="-|>",
+	                                  connectionstyle="angle3,angleA=-45,angleB=45",
+	                                  fc="w"), 
+	                  )
+
+	# Creating the annotations on the arrows
+
+	# Bio to Physics
+	props2 = dict(boxstyle='round', color=determine_facecolor(val[0]),alpha=1)
+	ax.text(0.59,0.63,str(val[0])+stringError[0],
+		size=14,ha='center', va='center',fontsize=14,
+		verticalalignment='top', bbox=props2,color='w',transform=ax.transAxes)
+
+	# Bio to Chem
+	props1 = dict(boxstyle='round', color=determine_facecolor(val[1]),alpha=1)
+	ax.text(0.41,0.63,str(val[1])+stringError[1],
+		size=14,ha='center', va='center', fontsize=14,
+		verticalalignment='top', bbox=props1,color='w',transform=ax.transAxes)
+
+	# Chem to Bio
+	props0 = dict(boxstyle='round', color=determine_facecolor(val[2]),alpha=1)
+	ax.text(0.32,0.7,str(val[2])+stringError[2],
+		size=14,ha='center', va='center', fontsize=14,
+		verticalalignment='top', bbox=props0,color='w',transform=ax.transAxes)
+
+	# Chem to Physics
+	props5 = dict(boxstyle='round', color=determine_facecolor(val[3]),alpha=1)
+	ax.text(0.51,0.3,str(val[3])+stringError[3],
+		size=14,ha='center', va='center', fontsize=14,
+		verticalalignment='top', bbox=props5,color='w',transform=ax.transAxes)
+
+
+	# Physics to Bio
+	props3 = dict(boxstyle='round', color=determine_facecolor(val[4]),alpha=1)
+	ax.text(0.68,0.7,str(val[4])+stringError[4],
+		size=14,ha='center', va='center', fontsize=14,
+		verticalalignment='top', bbox=props3,color='w',transform=ax.transAxes)
+
+	# Physics to Chem
+	props4 = dict(boxstyle='round', color=determine_facecolor(val[5]),alpha=1)
+	ax.text(0.51,0.52,str(val[5])+stringError[5],
+		size=14,ha='center', va='center', fontsize=14,
+		verticalalignment='top', bbox=props4,color='w',transform=ax.transAxes)
+
+
+	# Export and save the image
+	pylab.axis('off')
+	pylab.title(filename, fontsize=20)
+
+	pylab.savefig(rawPath+filename,dpi=300)
+	pylab.close()    
+
+
+def determine_facecolor(score):
+
+	## On a -1,0,1 point scale
+
+    fc ='grey'
+    if score < -0.1:
+        fc = 'red'
+    elif score>=-0.1 and score <0.1:
+        fc = 'orange'
+    elif score >=0.1:
+        fc ='green'
+
+    ## On a 5 point scale
+    
+    #fc ='grey'
+    #if score < 3.2:
+    #    fc = 'red'
+    #elif score>=3.2 and score <4.2:
+    #    fc = 'orange'
+    #elif score >=4.2:
+    #    fc ='green'
+
+    return fc
+
+### Firas Plots ###
+
+# Currently not really working, but not really used either
+
+"""
+
+def chooseData(dataSource = allData,
+               courseList = None,
+               course = None,
+               questionDict = None,
+               question = None):
+    
+    # First get the data for the Course Name (or all data)
+    
+    if course is None:
+        currD = dataSource
+    else:
+        currD = dataSource[dataSource['Course']==course]
+        
+    # Then get the data for the question specified        
+    if question is None:
+        result = currD
+    else:
+        result = currD[questionDict[question]]
+    
+    return result
+
+def createFirasPlot(pairedQs,
+                    course_interested,
+                    export_filename):
+
+    for i,q in enumerate(pairedQs.iteritems()):
+
+        subplot(1,3,i+1)
+
+        # Choosing the data first
+
+        A_B = chooseData(allData,
+                         courseList = courseList,
+                         course = course_interested,
+                         questionDict = questions,
+                         question = q[1][0])
+
+        B_A = chooseData(allData,
+                         courseList = courseList,
+                         course = course_interested,
+                         questionDict = questions,
+                         question = q[1][1])
+
+        # Filtering out data
+        A_B = A_B[A_B>0]
+        B_A = B_A[B_A>0]
+
+        gg = zip(A_B,B_A)
+
+        xL = []
+        yL = []
+        area = [] 
+        colour = []
+
+        for it in Counter(gg):
+
+            xL.append(it[0])
+            yL.append(it[1])
+            area.append(1e4*Counter(gg)[it]/len(gg))
+            colour.append((it[0] + it[1]))
+
+            text(it[0],it[1],
+                 math.trunc(round(100.*Counter(gg)[it]/len(gg),0)),
+                 size=13,horizontalalignment='center',verticalalignment='center',color='k')    
+
+        sct = scatter(xL, yL, s=area, c = colour, linewidths=2, edgecolor='k',cmap='jet')
+        sct.set_alpha(0.65)
+
+        xlim(.5,5.5)
+        ylim(.5,5.5)
+
+        pyplot.locator_params(axis = 'x', nbins = 6)
+        pyplot.locator_params(axis = 'y', nbins = 6)
+
+        xlabel('{0}'.format(q[1][0]),size=16)
+        ylabel('{0}'.format(q[1][1]),size=16)
+        title('{0}'.format(q[0]),size=18)
+        
+        pylab.suptitle(course_interested+'\n \n \n',size=24)
+
+        if i >2:
+            text(6,4,'1 - Strongly disagree \n5- Strongly agree',size=13)
+
+        savefig('/phd/TA/Year 3 - InterClass/python/export/Firas_plots.pdf')
+
+    pylab.tight_layout()    
+
+
+figsize(12,6)
+
+course_interested = 'Biol 121 Pre'
+
+pairedQs = {'Physics and Biology': ['P important for B','B important for P'],
+            'Biology and Chemistry': ['B important for C','C important for B'],
+            'Chemistry and Physics': ['C important for P','P important for C']}
+
+course_interested = 'Biol 121 Pre'
+createFirasPlot(pairedQs,course_interested,course_interested+'FirasPlot.pdf')
+
+
+"""
